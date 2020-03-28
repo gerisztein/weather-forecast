124ad0a19849c1707c9e19da2c83edbb8ddf0f02
Lucas Gerisztein <lucas.gerisztein@crvsh.io>
2020-03-28 23:00:05 +0100

    chore: helpers methods

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+export const filterData = ({ data }) => {
+  const [today, ...forecast] = data.list
+  const todayDate = today.dt_txt.split(' ')[0]
+  const days = 3
+  const list = forecast
+    .filter(day => day.dt_txt.includes('12:00:00') && !day.dt_txt.includes(todayDate))
+    .slice(0, days)
+
+  return {
+    city: data.city,
+    list,
+    today
+  }
+}
+
+export const formatTemperature = (temperature) => `${Math.round(temperature)}°`
+
+export const getWeekday = (date, locale = 'en-US') => new Date(date)
+  .toLocaleDateString(locale, { weekday: 'short' })
