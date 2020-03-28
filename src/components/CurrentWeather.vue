c2b6d11e82b665621f4631b72bb651e2710a2f20
Lucas Gerisztein <lucas.gerisztein@crvsh.io>
2020-03-28 23:00:42 +0100

    feat: current weather component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,119 @@
+<template lang="pug">
+  .CurrentWeather
+    h1.CurrentWeather-title Now
+
+    .CurrentWeather-details
+      .CurrentWeather-icon
+        img(
+          :alt="description"
+          :src="iconUrl"
+        )
+      .CurrentWeather-info
+        .CurrentWeather-location(v-text="location")
+        .CurrentWeather-description(v-text="description")
+        .CurrentWeather-temperature(v-text="temperature")
+        .CurrentWeather-feel feels like {{realFeelTemperature}}
+        .CurrentWeather-max H: {{maxTemperature}} L: {{minTemperature}}
+</template>
+
+<script>
+import { formatTemperature } from '@/utils'
+
+export default {
+  name: 'CurrentWeather',
+
+  props: {
+    city: {
+      default: '',
+      required: false,
+      type: String
+    },
+
+    country: {
+      default: '',
+      required: false,
+      type: String
+    },
+
+    description: {
+      default: '',
+      required: false,
+      type: String
+    },
+
+    icon: {
+      default: '',
+      required: false,
+      type: String
+    },
+
+    maxTemp: {
+      default: 0,
+      required: false,
+      type: Number
+    },
+
+    minTemp: {
+      default: 0,
+      required: false,
+      type: Number
+    },
+
+    realFeel: {
+      default: 0,
+      required: false,
+      type: Number
+    },
+
+    temp: {
+      default: 0,
+      required: false,
+      type: Number
+    }
+  },
+
+  data () {
+    return {
+      iconUrl: `http://openweathermap.org/img/wn/${this.icon}@2x.png`,
+      realFeelTemperature: formatTemperature(this.realFeel),
+      location: `${this.city}, ${this.country}`,
+      maxTemperature: formatTemperature(this.minTemp),
+      minTemperature: formatTemperature(this.maxTemp),
+      temperature: formatTemperature(this.temp)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.CurrentWeather
+  margin-bottom 32px
+  padding 16px
+
+  &-title
+    border-bottom 5px solid #efefef
+    display inline-block
+    font-size 2rem
+    margin-bottom 32px
+    padding 0 48px 0 12px
+
+  &-details
+    align-items top
+    display flex
+
+  &-info
+    margin-left 32px
+
+  &-description
+    font-size: .75rem
+    margin-bottom 8px
+    text-transform uppercase
+
+  &-location
+    font-size 1.5rem
+
+  &-temperature
+    font-size 2rem
+    font-weight 900
+    margin-bottom 8px
+</style>
